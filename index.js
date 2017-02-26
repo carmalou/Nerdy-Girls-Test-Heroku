87a232a06e97bdc86d528e67fe728e81e533ce3c
carmalou <carmenbourlonlong@gmail.com>
2017-02-26 15:29:32 -0600

    maybe define the port, huh?

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
